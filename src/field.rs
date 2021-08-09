499120958b15687c24d52bb2f2fa9eb1ce5125fd
toshiki goto <gotoshi.euphonium@gmail.com>
2021-08-09 21:50:26 +0900

    remove compile error

--- src/field.rs
+++ src/field.rs
@@ -3,22 +3,26 @@ use crate::piece::Shape;
 use rusttype::Point;
 use std::fmt;
 
+pub enum FieldElement {
+    ID(usize),
+    NONE,
+}
 pub struct Field {
-    field: Vec<Vec<i32>>,
+    field: Vec<Vec<FieldElement>>,
     pieces: Vec<Piece>,
 }
 
-pub enum FieldType {
+pub enum PieceGenSetting {
     ROTATE,
     MILLER,
-    ALL,
+    ALL, // rotate and miller
 }
 
 impl Field {
     pub fn new(field: &str) -> Field {
         // not immplemented
-        let field: Vec<Vec<i32>> = Vec::new();
-        let tmp: Vec<i32> = Vec::new();
+        let mut field: Vec<Vec<FieldElement>> = Vec::new();
+        let tmp: Vec<FieldElement> = Vec::new();
         field.push(tmp);
         let pieces: Vec<Piece> = Vec::new();
         Field {
@@ -26,22 +30,22 @@ impl Field {
             pieces: pieces,
         }
     }
-    pub fn make_field(field: &str, pieces: Vec<&str>, filed_type: FieldType) -> Field {
+    pub fn make_field(field: &str, pieces: Vec<&str>, filed_type: PieceGenSetting) -> Field {
         let mut field = Field::new(field);
         match filed_type {
-            ROTATE => {
+            PieceGenSetting::ROTATE => {
                 for (i, piece) in pieces.iter().enumerate() {
                     let rotate_pieces = Shape::from_str(piece).all_rotate(i);
                     field.append_piece(rotate_pieces);
                 }
             }
-            MILLER => {
+            PieceGenSetting::MILLER => {
                 for (i, piece) in pieces.iter().enumerate() {
                     let rotate_pieces = Shape::from_str(piece).all_miller(i);
                     field.append_piece(rotate_pieces);
                 }
             }
-            ALL => {
+            PieceGenSetting::ALL => {
                 for (i, piece) in pieces.iter().enumerate() {
                     let rotate_pieces = Shape::from_str(piece).all_rotate_and_miller(i);
                     field.append_piece(rotate_pieces);
@@ -54,33 +58,50 @@ impl Field {
         self.pieces.push(piece);
     }
 
-    pub fn find_empty(&self) -> Point<i32> {
-        //not immplemented
+    pub fn find_empty(&self) -> Option<Point<usize>> {
         // find empty place in field.field
         // return value is coordinate
         let field = &self.field;
-        Point { x: 0, y: 0 }
+        for (i, column) in field.iter().enumerate() {
+            for (j, elem) in column.iter().enumerate() {
+                if let FieldElement::NONE = elem {
+                    let p = Point { x: i, y: j };
+                    return Some(p);
+                }
+            }
+        }
+        None
     }
 
     pub fn can_place_piece_at_point(&self, piece: &Piece, p: &Point<i32>) -> bool {
+        // not immplemented
         // whether it can place the piece at the point in the field.
         true
     }
     pub fn place_piece(&mut self, piece: &Piece, p: &Point<i32>) {
+        // not immplemented
         // it should be done after can_place_piece.
     }
 
     pub fn get_piece(&mut self) -> Piece {
-        let piece = self.pieces.shapes.pop().unwrap();
+        let piece = self.pieces.pop().unwrap();
         piece
+    }
+}
+impl fmt::Display for FieldElement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::ID(n) => write!(f, "{}", n),
+            Self::NONE => write!(f, "*"),
+        }
     }
 }
 
 impl fmt::Display for Field {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         //
-        let s: String = String::new();
-        for line in self.field {
+        let mut s: String = String::new();
+        for line in &self.field {
             for id in line {
                 s = s + &id.to_string();
             }
--- src/piece.rs
+++ src/piece.rs
@@ -5,16 +5,16 @@ pub struct Shape(Vec<Vec<bool>>);
 pub struct Piece {
     // Piece includes Shapes which can be regarded as the same.
     shapes: Vec<Shape>, // include Rotational symmetry or Line symmetry of shape.
-    index: i32, // index of shapes. if index equal to len(shapes), this piece is not still used.
+    index: usize, // index of shapes. if index equal to len(shapes), this piece is not still used.
     id: usize,
-    coordinate: Point<i32>, // upper left coordinate of piece.
+    coordinate: Point<usize>, // upper left coordinate of piece.
 }
 
 impl Shape {
     // To generate shape
 
     pub fn from_str(s: &str) -> Shape {
-        // str must have same row length, have only 0,1,\n and each row and collom must have 1.
+        // str must have same row length, have only 0,1,\n and each row and column must have 1.
         // for example 110\n011\n, not 01\n001 , 011\n122\n or 011\n011\n;
         // if not, compiler must be going to panic!
         let split_newline: Vec<&str> = s.split("\n").collect();
@@ -26,88 +26,110 @@ impl Shape {
                 x.chars()
                     .collect::<Vec<char>>()
                     .iter()
-                    .map(|x| x == '1')
+                    .map(|x| x == &'1')
                     .collect::<Vec<bool>>()
             })
             .collect::<Vec<Vec<bool>>>();
         Shape::check_input_row_0(&split_all);
-        Shape::check_input_collom_0(&split_all);
+        Shape::check_input_column_0(&split_all);
 
         Shape(split_all)
     }
     pub fn check_input_length(v: &Vec<&str>) {
-        let leng: Vec<i32> = v
+        let mut leng: Vec<usize> = v
             .iter()
             .map(|&x| x.to_string().len())
-            .collect::<Vec<i32>>()
-            .sort();
+            .collect::<Vec<usize>>();
+        leng.sort();
         if leng[0] != leng[leng.len() - 1] {
             panic!("Value Error: Shape must be rectangle. So you should input str having the same length of row : {:?}",v);
         }
     }
     pub fn check_input_row_0(v: &Vec<Vec<bool>>) {
+        // not immplemented
         if true {
             panic!("ValueError: Shape must be optimize. So you should input str each of row must have 1.")
         }
     }
-    pub fn check_input_collom_0(v: &Vec<Vec<bool>>) {
+    pub fn check_input_column_0(v: &Vec<Vec<bool>>) {
+        // not immplemented
         if true {
-            panic!("ValueError: Shape must be optimize. So you should input str each of collom must have 1.")
+            panic!("ValueError: Shape must be optimize. So you should input str each of column must have 1.")
         }
     }
 
     // To generate Piece
     // ownership is going to move.
-    fn rotate(&self) -> Shape {}
+    fn rotate(&self) -> Shape {
+        // not implemented
+        let tmp: Vec<Vec<bool>> = Vec::new();
+        Shape(tmp)
+    }
+    fn miller<'a>(&self) -> Shape {
+        let mut miller_shape: Vec<Vec<bool>> = Vec::new();
+        for column in &self.0 {
+            let mut miller = column.clone();
+            miller.reverse();
+            miller_shape.push(miller);
+        }
+        Shape(miller_shape)
+    }
+
     pub fn all_rotate(self, id: usize) -> Piece {
         let mut shapes: Vec<Shape> = Vec::new();
         let mut rotate = self.rotate();
         let mut tmp;
-        for i in [0..3] {
+        for _ in 0..3 {
             tmp = rotate.rotate();
             shapes.push(rotate);
             rotate = tmp;
         }
         shapes.push(rotate);
-        Piece {
-            shapes: shapes,
-            index: 4,
-            id: id,
-            coordinate: Point { x: 0, y: 0 },
-        }
-    }
-    fn miller(&self) -> Shape {
-        &self.into_iter().map(|x| x.reverse()).collect()
+        Piece::new(shapes, id)
     }
     pub fn all_miller(self, id: usize) -> Piece {
         let mut shapes: Vec<Shape> = Vec::new();
-        shapes.push(&self.miller());
+        shapes.push(self.miller());
         shapes.push(self);
 
-        Piece {
-            shapes: shapes,
-            index: 2,
-            id: id,
-            coordinate: Point { x: 0, y: 0 },
-        }
+        Piece::new(shapes, id)
     }
     pub fn all_rotate_and_miller(self, id: usize) -> Piece {
-        self.all_rotate(id).add_piece(&mut self.all_miller(id))
+        let miller = self.all_miller(id);
+        let mut shapes: Vec<Shape> = Vec::new();
+        for sps in miller.shapes {
+            let mut rotate = sps.all_rotate(id);
+            shapes.append(&mut rotate.shapes);
+        }
+        Piece::new(shapes, id)
     }
 }
 
 impl Piece {
+    pub fn new(shapes: Vec<Shape>, id: usize) -> Piece {
+        let index = shapes.len();
+        let coordinate = Point { x: 0, y: 0 };
+        Piece {
+            shapes,
+            index,
+            id,
+            coordinate,
+        }
+    }
     // Piece -> Piece
-    pub fn translation(&self, p: Point<i32>) -> Piece {
+    pub fn translation(self, p: Point<usize>) -> Piece {
         Piece {
             shapes: self.shapes,
             index: self.index,
             id: self.id,
-            coordinate: p + self.coordinate,
+            coordinate: Point {
+                x: p.x + self.coordinate.x,
+                y: p.y + self.coordinate.y,
+            },
         }
     }
 
-    pub fn add_piece(self, other: &mut Piece) -> Piece {
+    pub fn add_piece(mut self, other: &mut Piece) -> Piece {
         // index is initialized.
         self.shapes.append(&mut other.shapes);
         self.index = self.shapes.len();
